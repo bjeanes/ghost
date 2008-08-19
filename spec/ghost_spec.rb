79413cf21d65add21d9bbf4f47e718686d62f270
Bodaniel Jeanes <me@bjeanes.com>
2008-08-19 14:31:16 +1000

    Adding a TON of specs because I want to do BDD as much as possible here. Not done yet but just committing so I don't accidently wipe everything and hate myself

--- spec/ghost_spec.rb
+++ spec/ghost_spec.rb
@@ -1,13 +1,105 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
+# Warning: these tests will delete all hostnames in the system. Please back them up first
+
 describe Host, ".list" do
+  after(:each) { Host.empty! }
   it "should return an array" do
     Host.list.should be_instance_of(Array)
+  end
+  
+  it "should contain instances of Host" do
+    Host.add('ghost-test-hostname.local')
+    Host.list.first.should be_instance_of(Host)
+  end
+  
+  it "should cache lists"
+end
+
+describe Host do
+  after(:each) { Host.empty! }
+  it "should have an IP" do
+    hostname = 'ghost-test-hostname.local'
+    
+    Host.add(hostname)
+    host = Host.list.first
+    host.ip.should eql('127.0.0.1')
+    
+    host.empty!
+    
+    ip = '169.254.23.121'
+    Host.add(hostname, ip)
+    host.ip.should eql(ip)
+  end
+  it "should have a hostname" do
+    hostname = 'ghost-test-hostname.local'
+    
+    Host.add(hostname)
+    host = Host.list.first
+    host.hostname.should eql(hostname)
+    
+    host.empty!
+    
+    ip = '169.254.23.121'
+    Host.add(hostname, ip)
+    host.hostname.should eql(hostname)
+  end
+  it ".to_s should return hostname" do
+    hostname = 'ghost-test-hostname.local'
+    
+    Host.add(hostname)
+    host = Host.list.first
+    host.to_s.should eql(hostname)
   end
 end
 
 describe Host, ".add" do
-  it "should return true" do
-    Host.add.should be_instance_of(Array)
+  after(:each) { Host.empty! }
+  it "should return true when passed hostname" do
+    Host.add('ghost-test-hostname.local').should be_true
+    Host.delete('ghost-test-hostname.local') # remove hostname when done
   end
+  
+  it "should not add duplicates" do
+    Host.empty!
+    Host.add('ghost-test-hostname.local')
+    Host.add('ghost-test-hostname.local')
+    Host.list.should have(1).thing
+  end
+  
+  it "should raise error if hostname already exists" do
+    Host.empty!
+    Host.add('ghost-test-hostname.local')
+    Host.add('ghost-test-hostname.local').should raise_error
+    Host.list.should have(1).thing
+  end
+  
+  it "should overwrite existing hostname if forced" do
+    hostname = 'ghost-test-hostname.local'
+    
+    Host.empty!
+    Host.add(hostname)
+    
+    Host.list.first.hostname.should eql(hostname)
+    Host.list.first.ip.should eql('127.0.0.1')
+    
+    Host.add(hostname, '10.0.0.1', true)
+    Host.list.first.hostname.should eql(hostname)
+    Host.list.first.ip.should eql('10.0.0.1')
+    
+    Host.list.should have(1).thing
+  end
+end
+
+describe Host, ".empty!" do
+  it "should empty the hostnames" do
+    Host.add('ghost-test-hostname.local') # add a hostname to be sure
+    Host.empty!
+    Host.list.should have(0).things
+  end
+end
+
+describe Host, ".backup and", Host, ".restore" do
+  it "should return a yaml file of all hosts and IPs when backing up"
+  it "should empty the hosts and restore only the ones in given yaml"
 end
