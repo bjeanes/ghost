dd8fba2a3ff557a8921cd8f668c3cbf4e8761db5
Bodaniel Jeanes <me@bjeanes.com>
2008-08-19 15:04:53 +1000

    added some Host.list caching code (untested)

--- lib/ghost/host.rb
+++ lib/ghost/host.rb
@@ -1,7 +1,17 @@
 class Host
   class << self
-    def list
-      `dscl localhost -list /Local/Default/Hosts`.split
+    def list(bypass_cache = false)
+      cmd = 'dscl localhost -list /Local/Default/Hosts'
+
+      if bypass_cache
+        @cache = `#{cmd}`.split
+      else
+        @cache ||= `#{cmd}`.split
+      end
+    end
+
+    def add(host, ip = "127.0.0.1")
+      `dscacheutil -flushcache`
     end
   end
 end
