4a85984b0bd471cf3514d8df0e0d7f52224a5633
Bodaniel Jeanes <me@bjeanes.com>
2008-08-19 12:01:27 +1000

    added some notes for the commands that the gem will wrap

---
+++ lib/ghost.rb
@@ -0,0 +1,10 @@
+=begin
+  showing hosts
+      dscl localhost -list /Local/Default/Hosts
+  showing all info for all hosts
+      dscl localhost -readall /Local/Default/Hosts
+  showing info for single host
+      dscl localhost -readall /Local/Default/Hosts/<hostname.local>
+  adding hostname
+      dscl localhost -read /Local/Default/Hosts/bne-developer.local
+=end
