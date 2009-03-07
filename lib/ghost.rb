d9f3136cfda66836fbd9efe935b335934865cb13
Mitchell Riley <mitchell.v.riley@gmail.com>
2009-03-07 15:12:40 +0800

    Add support for Linux
    Signed-off-by: Bodaniel Jeanes <me@bjeanes.com>

--- lib/ghost.rb
+++ lib/ghost.rb
@@ -1,2 +1,8 @@
 $: << File.dirname(__FILE__)
-require 'ghost/host'
+
+case RUBY_PLATFORM
+when /darwin/
+  require 'ghost/mac-host'
+when /linux/
+  require 'ghost/linux-host'
+end
---
+++ lib/ghost/linux-host.rb
@@ -0,0 +1,73 @@
+class Host
+  @@hosts_file = '/etc/hosts'
+  class << self
+    protected :new
+    
+    def list
+      entries = []
+      File.open(@@hosts_file).each do |line|
+        next if line =~ /^#/
+        if line =~ /^(\d+\.\d+\.\d+\.\d+) (.*)/
+          ip = $1
+          hosts = $2.split(' ')
+          hosts.each {|host| entries << Host.new(host, ip) }
+        end
+      end
+      entries
+    end
+
+    def add(host, ip = "127.0.0.1", force = false)
+      if find_by_host(host).nil? || force
+        delete(host)
+        new_host = Host.new(host, ip)
+        
+        hosts = list
+        hosts << new_host
+        write_out!(hosts)
+        
+        new_host
+      else
+        raise "Can not overwrite existing record"
+      end      
+    end
+    
+    def find_by_host(hostname)
+      list.find{|host| host.hostname == hostname }
+    end
+    
+    def find_by_ip(ip)
+      list.find_all{|host| host.ip == ip }
+    end
+    
+    def empty!
+      hosts = [Host.new("localhost", "127.0.0.1"),
+               Host.new(`hostname`, "127.0.0.1")]
+      write_out!(hosts)
+    end
+    
+    def delete(name)
+      hosts = list
+      hosts = hosts.delete_if {|host| host.name == name }
+      write_out!(hosts)
+    end
+
+    protected
+
+    def write_out!(hosts)
+      # Har har! inject!
+      output = hosts.inject("") {|s, h| s + "#{h.ip} #{h.hostname}\n" }
+      File.open(@@hosts_file, 'w') {|f| f.print output }
+    end
+  end
+
+  attr_reader :host, :ip
+
+  def initialize(host, ip)
+    @host = host
+    @ip = ip
+  end
+
+  alias :to_s :host
+  alias :name :host
+  alias :hostname :host
+end
