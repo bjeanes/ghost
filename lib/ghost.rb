b64a33e0fa7dcafde36d3be6a7d98f656baf5ff0
LarsFronius <l.fronius@googlemail.com>
2012-04-05 15:41:52 +0300

    Fix for obsolete "end" in ghost.rb

--- lib/ghost.rb
+++ lib/ghost.rb
@@ -3,7 +3,6 @@ $: << File.dirname(__FILE__)
 case RUBY_PLATFORM
 when /darwin/
   productVersion = `/usr/bin/sw_vers -productVersion`.strip
-  end
   if productVersion =~ /^10\.7\.[2-9]{1}$/
     require 'ghost/linux-host'
   else
