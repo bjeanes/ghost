d1077de56cffdf37f762293948749f9db50d79b2
Lars Fronius <lars@jimdo.com>
2012-04-05 14:03:52 +0200

    Fix for issue #14 in bjeanes/ghost. Looks up Mac OS X Version and uses /etc/hosts if equal or greater than 10.7.2

--- lib/ghost.rb
+++ lib/ghost.rb
@@ -2,7 +2,13 @@ $: << File.dirname(__FILE__)
 
 case RUBY_PLATFORM
 when /darwin/
-  require 'ghost/mac-host'
+  productVersion = `/usr/bin/sw_vers -productVersion`.strip
+  end
+  if productVersion =~ /^10\.7\.[2-9]{1}$/
+    require 'ghost/linux-host'
+  else
+    require 'ghost/mac-host'
+  end
 when /linux/
   require 'ghost/linux-host'
 end
