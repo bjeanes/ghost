f2557d5e0cc3dd164574f80d06d72069531929cd
Bodaniel Jeanes <me@bjeanes.com>
2012-04-15 21:04:21 -0500

    Host spec

--- lib/ghost/host.rb
+++ lib/ghost/host.rb
@@ -1,4 +1,8 @@
 module Ghost
   class Host < Struct.new(:name, :ip)
+    alias :to_s :name
+    alias :host :name
+    alias :hostname :name
+    alias :ip_address :ip
   end
 end
---
+++ spec/ghost/host_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper.rb")
+require 'ghost/host'
+
+describe Ghost::Host do
+  describe 'attributes' do
+    subject { Ghost::Host.new('google.com', '74.125.225.102') }
+
+    its(:name)       { should == 'google.com' }
+    its(:to_s)       { should == 'google.com' }
+    its(:host)       { should == 'google.com' }
+    its(:hostname)   { should == 'google.com' }
+    its(:ip)         { should == '74.125.225.102'}
+    its(:ip_address) { should == '74.125.225.102'}
+  end
+end
