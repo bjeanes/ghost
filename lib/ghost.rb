cf0c9a601b937dc0bff05eda3c5fe4bd3f598770
Bodaniel Jeanes <me@bjeanes.com>
2012-04-15 20:07:37 -0500

    Remove the old code with fire. Start fresh

--- lib/ghost.rb
+++ lib/ghost.rb
@@ -1,23 +1,7 @@
 $: << File.dirname(__FILE__)
 
-module Ghost
-  class RecordExists < StandardError
-  end
-end
-
-require 'rbconfig'
-
-case RbConfig::CONFIG['host_os']
-when /darwin/
-  productVersion = `/usr/bin/sw_vers -productVersion`.strip
-  if productVersion =~ /^10\.7\.[2-9]{1}$/
-    require 'ghost/linux-host'
-  else
-    require 'ghost/mac-host'
-  end
-when /linux/
-  require 'ghost/linux-host'
-end
-
-require 'ghost/ssh_config'
 require 'ghost/version'
+
+module Ghost
+end
+
