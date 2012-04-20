1ccb92a3b32ffd3ae249e68ea6de62dcfaaaf6d4
Bodaniel Jeanes <me@bjeanes.com>
2012-04-19 21:15:18 -0500

    Make it use actual /etc/hosts file instead of tempfile

--- lib/ghost/store.rb
+++ lib/ghost/store.rb
@@ -5,6 +5,5 @@ end
 require 'ghost/store/hosts_file_store'
 require 'ghost/store/dscl_store' # TODO: only load on OS X
 
-require 'tempfile'
-Ghost.store = Ghost::Store::HostsFileStore.new(Tempfile.new('hosts'))
+Ghost.store = Ghost::Store::HostsFileStore.new
 
