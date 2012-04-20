6cd41edf783be639027a5ee0d9b9c972d542dd34
Bodaniel Jeanes <me@bjeanes.com>
2012-04-19 21:32:01 -0500

    Fix CLI 'add' task parameters

--- lib/ghost/cli/task/add.rb
+++ lib/ghost/cli/task/add.rb
@@ -1,6 +1,6 @@
 Ghost::Cli.task :add do
-  def perform(*args)
-    host = Ghost::Host.new(*args.take(2))
+  def perform(host, ip = nil)
+    host = Ghost::Host.new(*[host, ip].compact)
     Ghost.store.add(host)
     puts "[Adding] #{host.name} -> #{host.ip}"
   end
