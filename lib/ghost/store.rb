265832f5c38ecf478d48522328259b6fbd6ade16
Bo Jeanes <me@bjeanes.com>
2012-09-04 09:49:39 -0500

    Don't rely on Ghost constant already existing for tests

--- lib/ghost/store.rb
+++ lib/ghost/store.rb
@@ -1,10 +1,12 @@
-class << Ghost
-  attr_accessor :store
+module Ghost
+  class << self
+    attr_accessor :store
+  end
 end
+
 
 require 'ghost/store/hosts_file_store'
 Ghost.store = Ghost::Store::HostsFileStore.new
 
 # TODO: only load on OS X and make it default when compatible
 require 'ghost/store/dscl_store'
-
