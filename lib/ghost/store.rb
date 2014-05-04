9a58bb6eaf0744b869e3f76b5bf6c877719db710
Bo Jeanes <me@bjeanes.com>
2014-05-04 00:20:44 -0700

    Remove dscl store

--- lib/ghost/store.rb
+++ lib/ghost/store.rb
@@ -7,6 +7,3 @@ end
 
 require 'ghost/store/hosts_file_store'
 Ghost.store = Ghost::Store::HostsFileStore.new
-
-# TODO: only load on OS X and make it default when compatible
-require 'ghost/store/dscl_store'
